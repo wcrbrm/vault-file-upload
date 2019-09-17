d4ce43281bcffc9ab3da3ec09ff4cd0c1d18e2af
Web Cerebrium <viclopata@gmail.com>
2019-09-17 22:30:51 +0300

    -dev-listen-address included

--- vault-dev-start.sh
+++ vault-dev-start.sh
@@ -2,4 +2,4 @@
 set -e
 echo 'xDNq4P1jJYF9Gm60GekUh8TW' > ~/.vault-token
 
-vault server -dev -dev-root-token-id="xDNq4P1jJYF9Gm60GekUh8TW"
+vault server -dev -dev-listen-address=0.0.0.0:8200  -dev-root-token-id="xDNq4P1jJYF9Gm60GekUh8TW"
